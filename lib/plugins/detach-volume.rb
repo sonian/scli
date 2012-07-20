e6096fc5e83673b0ff7db911e684f9522679373f
Josh Pasqualetto <josh.pasqualetto@sonian.net>
2012-07-20 13:31:54 -0700

    Do not require instance id for vol detach, find it from volume info.

--- lib/plugins/detach-volume.rb
+++ lib/plugins/detach-volume.rb
@@ -2,13 +2,14 @@ module Scli
   def self.detvol
     cli = MyCLI.new
     cli.parse_options
-    volume_id = cli.config[:volume_id]
+    volume_id = cli.config[:volume_id] || ARGV[1]
     instance_id = cli.config[:instance_id]
-    if volume_id.nil? || instance_id.nil?
-      puts "Instance and volume id's are required, please retry."
+    if volume_id.nil?
+      puts "Volume ID is required, please retry."
     else
-      server = Scli::Compute.new.servers.get(instance_id)
       volume = Scli::Storage.new.volumes.get(volume_id)
+      instance_id = volume.instance_id if instance_id.nil?
+      server = Scli::Compute.new.servers.get(instance_id)
       if server.nil?
         puts "Could not find server: #{instance_id}"
       elsif volume.nil?
@@ -26,4 +27,4 @@ module Scli
   end
 end
 
-PLUGINS << ["detvol", "detach-volume", "Volume ID (Req -v), Instance ID (Req -i)", "scli detvol -i 123456 -v 23456"]
+PLUGINS << ["detvol", "detach-volume", "Volume ID (Req -v), Instance ID (Opt -i)", "scli detvol 23456"]
